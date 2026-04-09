320fd44268e8a49f4e80825408d0bf4626017512
wbjang <wonbae623@naver.com>
2026-04-09 18:51:27 +0900

    다크 테마 적용 시 상태바, 네비게이션바도 적용되도록 수정

--- app/src/main/java/com/wbjang/footballdiary/MainActivity.kt
+++ app/src/main/java/com/wbjang/footballdiary/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.wbjang.footballdiary
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.wbjang.footballdiary.ui.main.MainViewModel
@@ -30,12 +38,29 @@ class MainActivity : ComponentActivity() {
                 ThemeMode.DARK -> true
                 ThemeMode.SYSTEM -> isSystemInDarkTheme()
             }
+
+            val view = LocalView.current
+            if (!view.isInEditMode) {
+                SideEffect {
+                    val window = (view.context as Activity).window
+                    WindowCompat.getInsetsController(window, view).apply {
+                        isAppearanceLightStatusBars = !isDark
+                        isAppearanceLightNavigationBars = !isDark
+                    }
+                }
+            }
+
             FootballDiaryTheme(darkTheme = isDark) {
-                val navController = rememberNavController()
-                NavGraph(
-                    navController = navController,
-                    startDestination = viewModel.startDestination
-                )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavGraph(
+                        navController = navController,
+                        startDestination = viewModel.startDestination
+                    )
+                }
             }
         }
     }
