a6e8bc38ca7cda10c3dc2126409c83c3a040c713
wbjang <wonbae623@naver.com>
2026-04-14 21:12:57 +0900

    앱 로그 태그 수정

--- app/src/main/java/com/wbjang/footballdiary/util/AppLogger.kt
+++ app/src/main/java/com/wbjang/footballdiary/util/AppLogger.kt
@@ -7,17 +7,17 @@ object AppLogger {
     var isEnabled: Boolean = BuildConfig.DEBUG
 
     fun d(tag: String, message: String) {
-        if (isEnabled) Log.d(tag, message)
+        if (isEnabled) Log.d("wbjang : $tag", message)
     }
 
     fun w(tag: String, message: String) {
-        if (isEnabled) Log.w(tag, message)
+        if (isEnabled) Log.w("wbjang : $tag", message)
     }
 
     fun e(tag: String, message: String, throwable: Throwable? = null) {
         if (isEnabled) {
-            if (throwable != null) Log.e(tag, message, throwable)
-            else Log.e(tag, message)
+            if (throwable != null) Log.e("wbjang : $tag", message, throwable)
+            else Log.e("wbjang : $tag", message)
         }
     }
 }
